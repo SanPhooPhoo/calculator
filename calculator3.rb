2cc8c62d81c0b210d083b4292c0a675f0440d8f3
sanphoophoo <sanphooktg@gmail.com>
2018-10-20 10:44:40 +0800

    calculator3

--- calculator3.rb
+++ calculator3.rb
@@ -1,32 +1,6 @@
-puts "Enter first number"
-first=gets.chomp.to_i
-puts "Enter second number"
-second=gets.chomp.to_i
-puts "Enter operation(+,-,*,/)"
-op=gets.chomp
-
-if
-op=="+"
-print "The answer is"
-puts first+second
-elsif
-op=="-"
-
-print "The answer is"
-puts first-second
-elsif
-op=="*"
-print "The answer is"
-puts first*second
-else
-print "The answer is"
-puts first/second
-end
 
 loop do
-puts "Do You want to do another(y/n)"
-i=gets.chomp
-if i=="y"
+
 puts "Enter first number"
 first=gets.chomp.to_i
 puts "Enter second number"
@@ -35,19 +9,24 @@ puts "Operation(+,-,*,/)"
 op=gets.chomp
 
 if op=="+"
+print " The result is "
 puts first+second
 
 elsif op=="-"
+print " The result is "
 puts first-second
 
 elsif op=="*"
+print " The result is "
 puts first*second
 
-else
+else op=="/"
+print " The result is "
 puts first/second
 end
-else
+puts "Do You want to do another(y/n)"
+i=gets.chomp
+if i!="y"
 break
 end
 end
-
